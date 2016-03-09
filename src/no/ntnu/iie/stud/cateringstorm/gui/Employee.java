67e5e6ac89a702be8bdcccbf15656666b348dff9
Audun Arbo <audunwar@student.hist.no>
2016-03-09 14:15:27 +0100

    Added employee GUI base

---
+++ src/no/ntnu/iie/stud/cateringstorm/gui/Employee.java
@@ -0,0 +1,30 @@
+package no.ntnu.iie.stud.cateringstorm.gui;
+
+import javax.swing.*;
+import javax.swing.table.TableColumn;
+
+/**
+ * Control panel UI for all employees
+ * Created by Audun on 09.03.2016.
+ */
+public class Employee extends JFrame {
+    private static final String WINDOW_TITLE = "Login";
+
+    // Window dimensions
+    private static final int WIDTH = 300;
+    private static final int HEIGHT = 250;
+    private JPanel mainPanel;
+    private JButton absenceButton;
+    private JTable timeTable;
+
+    public Employee() {
+        setTitle(WINDOW_TITLE);
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(mainPanel);
+    }
+
+    private void fillTimetable() {
+
+    }
+}
