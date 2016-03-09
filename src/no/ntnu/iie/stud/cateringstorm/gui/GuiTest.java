841b8d0146db35cd2ea16cf4c18dc3d4376f05c4
Audun Arbo <audunwar@student.hist.no>
2016-03-09 13:45:22 +0100

    Added login GUI base

---
+++ src/no/ntnu/iie/stud/cateringstorm/gui/GuiTest.java
@@ -0,0 +1,13 @@
+package no.ntnu.iie.stud.cateringstorm.gui;
+
+/**
+ * Created by Audun on 09.03.2016.
+ */
+public class GuiTest {
+    public static void main(String[] args) {
+        //LoginWindow loginWindowOld = new LoginWindow();
+        Login loginWindow = new Login();
+        //loginWindowOld.setVisible(true);
+        loginWindow.setVisible(true);
+    }
+}
