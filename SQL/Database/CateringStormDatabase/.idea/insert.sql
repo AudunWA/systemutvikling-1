fa978a4393a131ff78dfabf0e33b38fe926b1181
Echter <chrech.birr@gmail.com>
2016-03-14 13:12:40 +0100

    Updated employee with password and salt and updated dates

--- SQL/Database/CateringStormDatabase/.idea/insert.sql
+++ SQL/Database/CateringStormDatabase/.idea/insert.sql
@@ -7,13 +7,13 @@ INSERT INTO employee_type VALUES (5, 'sales person', 50);
 INSERT INTO employee_type VALUES (6, 'laundry staff', 60);
 
 -- (employee) --
-INSERT INTO employee VALUES (default, 'nootnoot', 'Knut', 'Kirkhorn', 'Pinguveien 23', '12345678', 'pingu@pingumail.com', 6);
-INSERT INTO employee VALUES (default, 'eliasbs', 'Elias', 'Sørensen', 'Valgrindvegen', '88888888', 'dragonslayer69@hotmail.com', 1);
-INSERT INTO employee VALUES (default, 'chechter', 'Christian', 'Echtermeyer', 'Stibakken 2', '99258217', 'chrech.birr@gmail.com', 4);
-INSERT INTO employee VALUES (default, 'pisken', 'Kenan', 'Mahic', 'Kolstad', '123232323', 'kenanpølser@pølsefest.no', 3);
-INSERT INTO employee VALUES (default, 'vebjørn', 'Audun', 'Arbo', 'Trondheim', '69696969', 'glassmesteren@knusemail.com', 2);
+INSERT INTO employee VALUES (default, 'nootnoot', 'Knut', 'Kirkhorn', 'Pinguveien 23', '12345678', 'pingu@pingumail.com', 6,'dd1a1d47e15b4e2165f597ded84c05640c4506ba30d76a7d25e7726af37dc41e','YWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFh');
+INSERT INTO employee VALUES (default, 'eliasbs', 'Elias', 'Sørensen', 'Valgrindvegen', '88888888', 'dragonslayer69@hotmail.com', 1,'dd1a1d47e15b4e2165f597ded84c05640c4506ba30d76a7d25e7726af37dc41e','YWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFh');
+INSERT INTO employee VALUES (default, 'chechter', 'Christian', 'Echtermeyer', 'Stibakken 2', '99258217', 'chrech.birr@gmail.com', 4,'dd1a1d47e15b4e2165f597ded84c05640c4506ba30d76a7d25e7726af37dc41e','YWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFh');
+INSERT INTO employee VALUES (default, 'pisken', 'Kenan', 'Mahic', 'Kolstad', '123232323', 'kenanpølser@pølsefest.no', 3,'dd1a1d47e15b4e2165f597ded84c05640c4506ba30d76a7d25e7726af37dc41e','YWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFh');
+INSERT INTO employee VALUES (default, 'vebjørn', 'Audun', 'Arbo', 'Trondheim', '69696969', 'glassmesteren@knusemail.com', 2,'dd1a1d47e15b4e2165f597ded84c05640c4506ba30d76a7d25e7726af37dc41e','YWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFh');
 INSERT INTO employee VALUES (default, 'drammen', 'Håvard', 'Høydalsnes', 'Drammen',
-'87654321', 'rave@ravemail.com', 5);
+'87654321', 'rave@ravemail.com', 5,'dd1a1d47e15b4e2165f597ded84c05640c4506ba30d76a7d25e7726af37dc41e','YWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFh');
 
 -- (employee types - includes all employee specialty tables) --
 INSERT INTO employee VALUES (1);
@@ -24,12 +24,12 @@ INSERT INTO chauffeur VALUES (5);
 INSERT INTO salesperson VALUES (6);
 
 -- (hours) --
-INSERT INTO hours VALUES (default, 2016-03-01, 8);
-INSERT INTO hours VALUES (default, 2016-03-01, 0);
-INSERT INTO hours VALUES (default, 2016-03-02, 8);
-INSERT INTO hours VALUES (default, 2016-03-02, 0);
-INSERT INTO hours VALUES (default, 2016-03-03, 8);
-INSERT INTO hours VALUES (default, 2016-03-03, 5);
+INSERT INTO hours VALUES (default, DATE('2016-03-01'), 8);
+INSERT INTO hours VALUES (default, DATE('2016-03-01'), 0);
+INSERT INTO hours VALUES (default, DATE('2016-03-02'), 8);
+INSERT INTO hours VALUES (default, DATE('2016-03-02'), 0);
+INSERT INTO hours VALUES (default, DATE('2016-03-03'), 8);
+INSERT INTO hours VALUES (default, DATE('2016-03-03'), 5);
 
 
 -- (employee_hours) --
@@ -54,11 +54,11 @@ INSERT INTO hours_employee VALUES (5,5);
 INSERT INTO hours_employee VALUES (6,5);
 
 -- (ingredient) --
-INSERT INTO ingredient VALUES (default, 2016-09-20, 'Pølse', 'AKA Kenans pølse', false, 2016-10-20);
-INSERT INTO ingredient VALUES (default, 2016-10-05, 'Ost', 'Det e jo nais da', true, 2016-10-16);
-INSERT INTO ingredient VALUES (default, 2016-10-10, 'Pizzabolle', 'The real mvp, premium, OG laken', true, 2016-10-19);
-INSERT INTO ingredient VALUES (default, 2016-10-20, 'Laks', 'fishy fishy', true, 2016-10-24);
-INSERT INTO ingredient VALUES (default, 2016-09-06, 'Bønner', 'Protein = Gøls', true, 2016-10-26);
+INSERT INTO ingredient VALUES (default, DATE('2016-09-20'), 'Pølse', 'AKA Kenans pølse', false, DATE('2016-10-20'));
+INSERT INTO ingredient VALUES (default, DATE('2016-10-05'), 'Ost', 'Det e jo nais da', true, DATE('2016-10-16'));
+INSERT INTO ingredient VALUES (default, DATE('2016-10-10'), 'Pizzabolle', 'The real mvp, premium, OG laken', true, DATE('2016-10-19'));
+INSERT INTO ingredient VALUES (default, DATE('2016-10-20'), 'Laks', 'fishy fishy', true, DATE('2016-10-24'));
+INSERT INTO ingredient VALUES (default, DATE('2016-09-06'), 'Bønner', 'Protein = Gøls', true, DATE('2016-10-26'));
 
 -- (dish_type) --
 INSERT INTO dish_type VALUES (default, 'Forrett');
@@ -106,6 +106,9 @@ INSERT INTO dish_package VALUES (1,3);
 INSERT INTO dish_package VALUES (3,3);
 INSERT INTO dish_package VALUES (6,3);
 
+-- (subscription) --
+
+
 -- (area) --
 INSERT INTO area VALUES (default, 'Kolstad');
 INSERT INTO area VALUES (default, 'Byåsen');
@@ -123,21 +126,15 @@ INSERT INTO customer VALUES (default, 'Berntsen', 'Bernt', 'Byåsvegen 4', 2);
 INSERT INTO customer VALUES (default, 'Arnsen', 'Gustaff', 'Kolstadvegen 3', 1);
 INSERT INTO customer VALUES (default, 'Olafdottir', 'Fredrikke', 'Hormonvegen 2', 1);
 
-
--- (subscription) --
-INSERT INTO subcsription VALUES (default
-
--- (recurring_order) --
-INSERT INTO recurring_order VALUES(default, 2, 36000, 1);
-
+INSERT INTO subcsription VALUES (default, DATE('2016-03-10'), DATE('2016-03-30'),
 -- (order) --
 
-INSERT INTO _order VALUES (default, 'sunn ordre', 2016-03-12, 2016-03-10, 3, false,  6, 1,null);
-INSERT INTO _order VALUES (default, 'kenans spesial', 2016-03-17, 2016-03-09, 3, false,  6,2,null);
-INSERT INTO _order VALUES (default, 'balansert ordre', 2016-03-12, 2016-03-09, 3, false,  6, 3, null);
-INSERT INTO _order VALUES (default, 'ekstra sunn ordre', 2016-03-17, 2016-03-08, 3, false,  6, 4, null);
-INSERT INTO _order VALUES (default, 'pølsefest', 2016-03-11, 2016-03-10, 3, false,  6, 5, null);
-INSERT INTO _order VALUES (default, 'helt super ordre', 2016-03-22, 2016-02-10, 3, false,  6, 6, null);
+INSERT INTO _order VALUES (default, 'sunn ordre', DATE('2016-03-12'), DATE('2016-03-10'), 3, false,  6, 1,null);
+INSERT INTO _order VALUES (default, 'kenans spesial', DATE('2016-03-17'), DATE('2016-03-09'), 3, false,  6,2,null);
+INSERT INTO _order VALUES (default, 'balansert ordre', DATE('2016-03-12'), DATE('2016-03-09'), 3, false,  6, 3, null);
+INSERT INTO _order VALUES (default, 'ekstra sunn ordre', DATE('2016-03-17'), DATE('2016-03-08'), 3, false,  6, 4, null);
+INSERT INTO _order VALUES (default, 'pølsefest', DATE('2016-03-11'), DATE('2016-03-10'), 3, false,  6, 5, null);
+INSERT INTO _order VALUES (default, 'helt super ordre', DATE('2016-03-22'), DATE('2016-02-10'), 3, false,  6, 6, null);
 
 -- (_order_package) --
 
