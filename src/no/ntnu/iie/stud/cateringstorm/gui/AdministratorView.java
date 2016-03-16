edf3776ab1d3944a9a8c9ff414b648877137051c
Kenan Mahic <kenanm@student.hist.no>
2016-03-16 12:45:55 +0100

    Added Administrator overview

---
+++ src/no/ntnu/iie/stud/cateringstorm/gui/AdministratorView.java
@@ -0,0 +1,23 @@
+package no.ntnu.iie.stud.cateringstorm.gui;
+
+import javax.swing.*;
+
+/**
+ * Created by kenan on 16.03.2016.
+ */
+public class AdministratorView extends JFrame{
+    private static final String WINDOW_TITLE = "Employee Overview";
+
+    private static final int WIDTH = 300;
+    private static final int HEIGHT = 250;
+    private JPanel mainPanel;
+    private JLabel administratorOverview;
+    private JList list1;
+
+    public AdministratorView() {
+        setTitle(WINDOW_TITLE);
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(mainPanel);
+    }
+}
