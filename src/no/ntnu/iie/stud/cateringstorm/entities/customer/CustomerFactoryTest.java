111e9f1575915981ff0ba37d0185ed34a1da17f3
Echter <chrech.birr@gmail.com>
2016-03-17 14:31:51 +0100

    2.0

---
+++ src/no/ntnu/iie/stud/cateringstorm/entities/customer/CustomerFactoryTest.java
@@ -0,0 +1,10 @@
+package no.ntnu.iie.stud.cateringstorm.entities.customer;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Chris on 17.03.2016.
+ */
+public class CustomerFactoryTest {
+
+}
