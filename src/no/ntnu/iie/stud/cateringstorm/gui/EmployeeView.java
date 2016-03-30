8ce2b4bc148937cf1cb27f312603fbdafd958a84
Audun Arbo <audunwar@student.hist.no>
2016-03-30 15:13:38 +0200

    GUI updates for login and employee

--- src/no/ntnu/iie/stud/cateringstorm/gui/EmployeeView.java
+++ src/no/ntnu/iie/stud/cateringstorm/gui/EmployeeView.java
@@ -1,6 +1,7 @@
 package no.ntnu.iie.stud.cateringstorm.gui;
 
 import no.ntnu.iie.stud.cateringstorm.entities.employee.Employee;
+import no.ntnu.iie.stud.cateringstorm.entities.employee.EmployeeType;
 
 import javax.swing.*;
 import javax.swing.table.TableColumn;
@@ -14,11 +15,15 @@ public class EmployeeView extends JFrame {
     private static final String WINDOW_TITLE = "Employee";
 
     // Window dimensions
-    private static final int WIDTH = 300;
+    private static final int WIDTH = 500;
     private static final int HEIGHT = 250;
     private JPanel mainPanel;
-    private JButton absenceButton;
-    private JTable timeTable;
+    private JButton chefButton;
+    private JButton chauffeurButton;
+    private JButton salespersonButton;
+    private JButton administratorButton;
+    private JButton expertButton;
+    private JLabel userTextLabel;
 
     // Data
     private Employee employee;
@@ -30,11 +35,56 @@ public class EmployeeView extends JFrame {
         setSize(WIDTH, HEIGHT);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setContentPane(mainPanel);
-    }
-    private void createUIComponents(){
+        displayButtons();
 
-    }
-    private void fillTimetable() {
+        userTextLabel.setText("Welcome back, " + employee.getFullName());
 
+        pack();
+    }
+
+    /**
+     * Displays the appropiate access buttons for the employee
+     */
+    private void displayButtons() {
+        // First hide everything employee specific
+        chefButton.setVisible(false);
+        chauffeurButton.setVisible(false);
+        expertButton.setVisible(false);
+        salespersonButton.setVisible(false);
+        administratorButton.setVisible(false);
+
+        switch (employee.getEmployeeType()) {
+            case EMPLOYEE:
+                break;
+            case CHEF:
+                chefButton.setVisible(true);
+                break;
+            case CHAUFFEUR:
+                chauffeurButton.setVisible(true);
+                break;
+            case NUTRITION_EXPERT:
+                expertButton.setVisible(true);
+                break;
+            case ADMINISTRATOR:
+                chefButton.setVisible(true);
+                chauffeurButton.setVisible(true);
+                expertButton.setVisible(true);
+                salespersonButton.setVisible(true);
+                administratorButton.setVisible(true);
+                break;
+            case SALESPERSON:
+                salespersonButton.setVisible(true);
+                break;
+        }
+    }
+
+    /***
+     * Test program
+     * @param args
+     */
+    public static void main(String[] args) {
+        EmployeeView view = new EmployeeView(new Employee(-1, "Test", "Forename", "Surname", "Address",
+                "Phone", "Email", EmployeeType.ADMINISTRATOR));
+        view.setVisible(true);
     }
 }
