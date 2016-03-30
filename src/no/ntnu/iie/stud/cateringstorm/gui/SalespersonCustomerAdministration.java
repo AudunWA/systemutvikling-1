3a2404141f7f6f4809cd9773ca33c5a3dd910e42
Kenan Mahic <kenanm@student.hist.no>
2016-03-30 12:52:22 +0200

    Laget Salesperson

---
+++ src/no/ntnu/iie/stud/cateringstorm/gui/SalespersonCustomerAdministration.java
@@ -0,0 +1,16 @@
+package no.ntnu.iie.stud.cateringstorm.gui;
+
+import javax.swing.*;
+
+/**
+ * Created by kenan on 30.03.2016.
+ */
+public class SalespersonCustomerAdministration {
+    private JPanel mainPanel;
+    private JButton addCustomerButton;
+    private JButton editCustomerButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
