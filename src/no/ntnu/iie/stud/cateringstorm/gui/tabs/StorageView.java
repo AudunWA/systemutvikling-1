47e100a8240c06dc1af26cdffe84ce18a63deee2
Audun Arbo <audunwar@student.hist.no>
2016-04-01 13:53:19 +0200

    Added base for storage GUI

---
+++ src/no/ntnu/iie/stud/cateringstorm/gui/tabs/StorageView.java
@@ -0,0 +1,15 @@
+package no.ntnu.iie.stud.cateringstorm.gui.tabs;
+
+import javax.swing.*;
+
+/**
+ * GUI for displaying the inventory of ingredients to chefs and nutrition experts(?).
+ * Lets the user add, edit and remove ingredients.
+ * Created by Audun on 01.04.2016.
+ */
+public class StorageView {
+    private JPanel mainPanel;
+    private JTable table1;
+    private JButton incrementSupply;
+    private JTextField textField1;
+}
