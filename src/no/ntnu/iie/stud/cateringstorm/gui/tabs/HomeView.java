e5b9657e582aa61119f63cc0d9b1c362c6e1ec5b
Audun Arbo <audunwar@student.hist.no>
2016-04-01 14:31:54 +0200

    Made changes to DashboardView, added HomeView.

---
+++ src/no/ntnu/iie/stud/cateringstorm/gui/tabs/HomeView.java
@@ -0,0 +1,16 @@
+package no.ntnu.iie.stud.cateringstorm.gui.tabs;
+
+import javax.swing.*;
+
+/**
+ * GUI for the employee home screen.
+ * Displays useful information and options for the employee.
+ * Created by Audun on 01.04.2016.
+ */
+public class HomeView extends JPanel {
+    private JPanel mainPanel;
+
+    public HomeView() {
+        add(mainPanel);
+    }
+}
