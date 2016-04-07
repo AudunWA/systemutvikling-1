1f47e8ed9c7da73702469583be61abddd93220a2
Elias <eliassorr@gmail.com>
2016-04-07 12:41:57 +0200

    Updated comments in AddCustomerDialog

--- src/no/ntnu/iie/stud/cateringstorm/gui/dialogs/AddCustomerDialog.java
+++ src/no/ntnu/iie/stud/cateringstorm/gui/dialogs/AddCustomerDialog.java
@@ -85,7 +85,9 @@ public class AddCustomerDialog extends JDialog{
             JOptionPane.showMessageDialog(this, "Please fill in an email.");
             return;
         }
-        Customer customer = new Customer(100,forename,surname,address,true,phone,email);//Inserted ID should be default as it's generated later
+
+        // FIXME: Inserted ID should be default as it's generated later
+        Customer customer = new Customer(100,forename,surname,address,true,phone,email);
 
         customer = CustomerFactory.createCustomer(customer);
 
@@ -99,7 +101,6 @@ public class AddCustomerDialog extends JDialog{
     }
 
     private void onCancel() {
-// add your code here if necessary
         dispose();
     }
     public static void main(String[] args) {
