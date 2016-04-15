55cb4b7d5c5289d9783f50b6e03c63c7561c3155
Elias <eliassorr@gmail.com>
2016-04-15 11:43:48 +0200

    Fixed dummy return value to evade nullpointerexception

--- src/no/ntnu/iie/stud/cateringstorm/entities/hours/HoursFactory.java
+++ src/no/ntnu/iie/stud/cateringstorm/entities/hours/HoursFactory.java
@@ -3,6 +3,7 @@ package no.ntnu.iie.stud.cateringstorm.entities.hours;
 import no.ntnu.iie.stud.cateringstorm.entities.employee.Employee;
 import no.ntnu.iie.stud.cateringstorm.util.GlobalStorage;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 
 /**
@@ -14,8 +15,9 @@ public final class HoursFactory {
     public static ArrayList<Hours> getAllHours(){
         // TODO: Implement method gathering all hours from table, only where employee ID equals this ID
         Employee thisEmployee = GlobalStorage.getLoggedInEmployee(); //Access to the respective employee
-
-        return null;
+        ArrayList<Hours> dummy = new ArrayList<>();
+        dummy.add(new Hours(1,1,new Timestamp(1460713371),new Timestamp(1460714371)));
+        return dummy;
     }
 
 }
