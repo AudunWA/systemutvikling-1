cddc421fee70593b838cc87d9c4f78aa549f7c27
Elias <eliassorr@gmail.com>
2016-04-18 15:40:15 +0200

    Added functionality to HoursFactory

--- src/no/ntnu/iie/stud/cateringstorm/entities/hours/HoursFactory.java
+++ src/no/ntnu/iie/stud/cateringstorm/entities/hours/HoursFactory.java
@@ -1,9 +1,10 @@
 package no.ntnu.iie.stud.cateringstorm.entities.hours;
 
+import no.ntnu.iie.stud.cateringstorm.database.Database;
 import no.ntnu.iie.stud.cateringstorm.entities.employee.Employee;
 import no.ntnu.iie.stud.cateringstorm.util.GlobalStorage;
 
-import java.sql.Timestamp;
+import java.sql.*;
 import java.util.ArrayList;
 
 /**
@@ -11,13 +12,51 @@ import java.util.ArrayList;
  */
 public final class HoursFactory {
 
-    // TODO: Implement factory class here
+    /**
+     * Creates customer from result.
+     * @param result
+     * @return Hours
+     * @throws SQLException
+     */
+    private static Hours createTimesheetFromResultSet(ResultSet result) throws SQLException{
+
+        int hoursId = result.getInt("hours_id");
+        int employeeId = result.getInt("employee_id");
+        Timestamp fromTime = result.getTimestamp("from_time");
+        Timestamp toTime = result.getTimestamp("to_time");
+        boolean active = result.getBoolean("active");
+        return new Hours(hoursId,employeeId,fromTime,toTime,active);
+
+    }
+
+    /**
+     * Creates an arraylist containing Hours
+     * @return ArrayList<Hours>
+     */
     public static ArrayList<Hours> getAllHours(){
         // TODO: Implement method gathering all hours from table, only where employee ID equals this ID
+        ArrayList<Hours> hoursList = new ArrayList<>();
         Employee thisEmployee = GlobalStorage.getLoggedInEmployee(); //Access to the respective employee
-        ArrayList<Hours> dummy = new ArrayList<>();
+
+        try (Connection connection = Database.getConnection()){
+            try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM hours")){
+                statement.executeQuery();
+
+                try (ResultSet result = statement.getResultSet()){
+                    while (result.next()){
+
+                        hoursList.add(createTimesheetFromResultSet(result));
+                    }
+                    return hoursList;
+                }
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+        /*ArrayList<Hours> dummy = new ArrayList<>();
         dummy.add(new Hours(1,1,new Timestamp(1460713371),new Timestamp(1460714371),true));
-        return dummy;
+        return dummy;*/
     }
 
 }
