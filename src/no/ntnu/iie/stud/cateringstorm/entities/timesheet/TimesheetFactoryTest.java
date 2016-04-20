010660a4b92d9c6920dd402e358d67be78222386
Elias <eliassorr@gmail.com>
2016-04-20 13:33:56 +0200

    Added current date checking in TimesheetView. Generated emtpy test class for TimesheetFactory. Shit

--- src/no/ntnu/iie/stud/cateringstorm/entities/timesheet/TimesheetFactoryTest.java
+++ src/no/ntnu/iie/stud/cateringstorm/entities/timesheet/TimesheetFactoryTest.java
@@ -1,7 +1,49 @@
 package no.ntnu.iie.stud.cateringstorm.entities.timesheet;
 
+import org.junit.Test;
+
 /**
  * Created by EliasBrattli on 14/04/2016.
  */
 public class TimesheetFactoryTest {
+
+    @Test
+    public void testGetAllTimesheets() throws Exception {
+
+    }
+
+    @Test
+    public void testGetTimesheetsByEmployee() throws Exception {
+
+    }
+
+    @Test
+    public void testGetActiveTimesheetsByEmployee() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateTimesheet() throws Exception {
+
+    }
+
+    @Test
+    public void testEditTimesheetStatus() throws Exception {
+
+    }
+
+    @Test
+    public void testGetUnfinishedTimeSheet() throws Exception {
+
+    }
+
+    @Test
+    public void testGetLatestTimeSheet() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateTimesheet() throws Exception {
+
+    }
 }
