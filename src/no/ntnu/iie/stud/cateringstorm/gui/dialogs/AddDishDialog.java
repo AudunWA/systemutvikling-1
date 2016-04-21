516ffdbb82d05c8d4df73f189fd2869eebab9a65
Kenan Mahic <kenanm@student.hist.no>
2016-04-21 14:53:59 +0200

    Added tables

--- src/no/ntnu/iie/stud/cateringstorm/gui/dialogs/AddDishDialog.java
+++ src/no/ntnu/iie/stud/cateringstorm/gui/dialogs/AddDishDialog.java
@@ -2,13 +2,16 @@ package no.ntnu.iie.stud.cateringstorm.gui.dialogs;
 
 import no.ntnu.iie.stud.cateringstorm.entities.dish.Dish;
 import no.ntnu.iie.stud.cateringstorm.entities.dish.DishFactory;
+import no.ntnu.iie.stud.cateringstorm.entities.ingredient.Ingredient;
+import no.ntnu.iie.stud.cateringstorm.gui.tablemodels.EntityTableModel;
 
 import javax.swing.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 
 public class AddDishDialog extends JDialog {
     private JPanel mainPanel;
-    private JButton okButton;
+    private JButton addButton;
     private JButton cancelButton;
     private JTextField dishName;
     private JTextField dishDescription;
@@ -16,16 +19,24 @@ public class AddDishDialog extends JDialog {
     private JComboBox dishType;
     private JLabel dishNameLabel;
     private JLabel dishDescriptionLabel;
+    private JTable addedTable;
+    private JButton okButton;
+    private JTable ingredientTable;
+
+    private ArrayList<Ingredient> addedList;
+    private ArrayList<Ingredient> ingredientList;
+    private ArrayList<Dish> dishes = new ArrayList<>();
+
 
 
     public AddDishDialog() {
         setContentPane(mainPanel);
         setModal(true);
-        getRootPane().setDefaultButton(okButton);
+        getRootPane().setDefaultButton(addButton);
 
-        okButton.addActionListener(new ActionListener() {
+        addButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                onOK();
+                onAdd();
             }
         });
 
@@ -34,6 +45,17 @@ public class AddDishDialog extends JDialog {
                 onCancel();
             }
         });
+        okButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) { onOk(); }
+        });
+        addedTable.getSelectionModel().addListSelectionListener(e -> {
+
+        });
+        ingredientTable.getSelectionModel().addListSelectionListener(e -> {
+
+        });
+
+
 
 // call onCancel() when cross is clicked
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
@@ -64,7 +86,7 @@ public class AddDishDialog extends JDialog {
         activeStatus.setSelectedIndex(0);
     }
 
-    private void onOK() {
+    private void onAdd() {
         String name = dishName.getText();
         if (name.isEmpty()) {
             JOptionPane.showMessageDialog(this, "Please fill in a name.");
@@ -81,16 +103,64 @@ public class AddDishDialog extends JDialog {
 
         boolean active = activeStatus.getSelectedIndex()<1;
 
+        ArrayList<Integer> test = new ArrayList<>();
+        test.add(ingredientTable.getSelectedRow() + 1);
 
-        Dish dish = DishFactory.createDish(name, description, type, active);
+        dishes.add(new Dish(0, name, description, type, active));
 
-        if (dish == null) {
-            JOptionPane.showMessageDialog(this, "An error occurred, please try again later.");
-        } else {
-            // Debug code
-            JOptionPane.showMessageDialog(this, dish);
+        int check = DishFactory.getAllDishes().size();
+
+        ArrayList<Ingredient> ingredients = new ArrayList<>();
+        for (int k = 0; k < addedList.size(); k++) {
+            ingredients.add(addedList.get(k));
         }
-        dispose();
+
+        if (ingredients.size() < 1) {
+            JOptionPane.showMessageDialog(this, "Please add package(s)");
+            return;
+        }
+
+        Dish dish = DishFactory.createDish(name, description, type, active, ingredients);
+
+        if (DishFactory.getAllDishes().size() > check) {
+            JOptionPane.showMessageDialog(this, "Add successful");
+            addedList = new ArrayList<>();
+            ((EntityTableModel) addedTable.getModel()).setRows(addedList);
+        }
+        JOptionPane.showMessageDialog(this, dish);
+    }
+
+    private void onOk() {
+        if (ingredientTable.getSelectedRow() > -1 && addedTable.getSelectedRow() > -1) {
+            JOptionPane.showMessageDialog(this, "Both tables selected. please deselect one by pressing with crtl");
+            ingredientTable.clearSelection();
+            addedTable.clearSelection();
+        } else {
+            if (ingredientTable.getSelectedRow() > -1) {
+                boolean check = true;
+                for (Ingredient ingredients : addedList) {
+                    if (ingredients.getIngredientId() == (ingredientTable.getSelectedRow() + 1)) {
+                        check = false;
+                    }
+                }
+                if (check) {
+                    addedList.add(ingredientList.get(ingredientTable.getSelectedRow()));
+                    ((EntityTableModel) addedTable.getModel()).setRows(addedList);
+                    ingredientTable.clearSelection();
+                } else {
+                    JOptionPane.showMessageDialog(this, "This ingredient is already added.");
+                    ingredientTable.clearSelection();
+                }
+            } else if (addedTable.getSelectedRow() > -1 && !ingredientTable.isColumnSelected(1) && !ingredientTable.isColumnSelected(2)) {
+                addedList.remove(addedTable.getSelectedRow());
+                ((EntityTableModel) addedTable.getModel()).setRows(addedList);
+                addedTable.clearSelection();
+            } else {
+                JOptionPane.showMessageDialog(this, "Please unselect the ingredient list. Do this by clicking with ctrl down.");
+                addedTable.clearSelection();
+            }
+        }
+
     }
 
     private void onCancel() {
